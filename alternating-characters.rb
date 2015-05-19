0840247a1e67f8f9140ba34fe2328cb526b84ba7
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-05-19 20:22:45 -0300

    Adding alternating-characters with OO

--- alternating-characters.rb
+++ alternating-characters.rb
@@ -1,6 +1,13 @@
-n = gets.to_i
-for i in 0..n-1
-  strings = gets
-  squeezed = strings.squeeze
-  puts strings.length - squeezed.length
+class Alternator
+  attr_accessor :n
+
+  def read_strings
+    @n = gets.to_i
+    for i in 0..@n-1
+      strings = gets.chomp.to_s
+      squeezed = strings.squeeze
+      puts strings.length - squeezed.length
+    end
+  end
+
 end
